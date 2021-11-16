e5f43cf22f39d65ba1cbceb701aced83161401ec
Stefan Dimitrov <54206891+StefanDimitrovDimitrov@users.noreply.github.com>
2021-11-16 18:36:46 +0200

    PostgreSQL integration

--- venty/venty/settings.py
+++ venty/venty/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'test_db'
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
@@ -72,14 +74,24 @@ WSGI_APPLICATION = 'venty.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+#
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'venty_db',
+        'USER': 'postgres',
+        'PASSWORD': 'admin',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
-
 
 
 # Password validation
