463dcbc3a00f4b61176976cee2e4e72774f822b7
Stefan Galabov <GalabovS@aforbes.com>
2021-11-19 17:04:55 +0200

    removed test_db and added new requirements

--- venty/venty/settings.py
+++ venty/venty/settings.py
@@ -31,14 +31,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'test_db'
-    'rest_framework'
+    'django.contrib.staticfiles'
 ]
 
 MIDDLEWARE = [
