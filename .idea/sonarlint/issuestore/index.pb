c75077e0507232a3c783a9b7775eb70ca7d119ac
Stefan Dimitrov <54206891+StefanDimitrovDimitrov@users.noreply.github.com>
2022-02-04 10:27:07 +0200

    bugfix

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -3,5 +3,5 @@
 users/apps.py,5\4\547f374cbbeef99e636bc50f0d5b5ba07651acb5
 M
 events/migrations/__init__.py,1\0\1067ce2f5f9dc255e995addf22630a421743aabe
-=
-users/urls.py,7\e\7ebb6dc32a2768a7e73ce1c83409f9c85bd4ad6a
+@
+guests/models.py,7\6\7695ba53809595bffaa4b1c87fd680b42cc4d637
--- guests/urls.py
+++ guests/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from guests import views
 
 urlpatterns = [
-    path('events/<int:pk>/guests', views.EventGuestGetCreate.as_view()),
-    path('events/<int:pk>/users', views.EventGuestCatalogUsers.as_view()),
-    path('events/<int:pk>/guests/<int:guest_pk>', views.EventGuestUpdate.as_view()),
+    path('<int:pk>/guests', views.EventGuestGetCreate.as_view()),
+    path('<int:pk>/users', views.EventGuestCatalogUsers.as_view()),
+    path('<int:pk>/guests/<int:guest_pk>', views.EventGuestUpdate.as_view()),
 ]
